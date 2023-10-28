96567e2fca680a0db0b73b4c2acfcda573baa14e
Eugene Clark <clarkjr2016@gmail.com>
2023-10-27 22:58:40 -0400

    adding more functiinality

--- unitTests.js
+++ unitTests.js
@@ -3,3 +3,42 @@ export function capitalize(word) {
   const capitalizedWord = firstLetter + word.slice(1); // adds the extracted first letter and the rest of the text together
   return capitalizedWord; // return the capitilized word
 }
+
+export function reverseString(string) {
+  const arrayOfString = string.split(""); //splits the string into an array
+  const reverseArray = arrayOfString.reverse(); //reverses the array
+  return reverseArray.join(""); //
+}
+
+export const calculator = {
+  add: (a, b) => {
+    return a + b;
+  },
+
+  subtract: (a, b) => {
+    return a - b;
+  },
+
+  multiply: (a, b) => {
+    return a * b;
+  },
+
+  divide: (a, b) => {
+    return a / b;
+  },
+};
+
+export function ceaserCipher(string, shift) {
+  //take in user string and for each character move it by a user specified amount.
+  //grab the string and turn it into an array
+  const characters = string.split("");
+
+  const charCodes = function (array) {
+    for (let i = 0; i < array.length; i++) {
+      array[i] = array[i].charCodeAt(0) + 1; //this changes the elements into ASCII characters and then shifts them by 1
+    }
+    return array;
+  }; //
+}
+
+ceaserCipher("abc");
--- unitTests.spec.js
+++ unitTests.spec.js
@@ -1,5 +1,29 @@
-import { capitalize } from "./unitTests";
+import { capitalize, reverseString, calculator } from "./unitTests";
 
-test("Capitalizes eugene to Eugene", () => {
-  expect(capitalize("eugene")).toBe("Eugene");
+test("Capitalizes hello to Hello", () => {
+  expect(capitalize("hello")).toBe("Hello");
+});
+
+test("howdy", () => {
+  expect(reverseString("howdy")).toBe("ydwoh");
+});
+
+test("add", () => {
+  expect(calculator.add(2, 5)).toBe(7);
+});
+
+test("subtract", () => {
+  expect(calculator.subtract(3, 1)).toBe(2);
+});
+
+test("multiply", () => {
+  expect(calculator.multiply(2, 5)).toBe(10);
+});
+
+test("divide", () => {
+  expect(calculator.divide(9, 3)).toBe(3);
+});
+
+test("a letter can be shifted to a letter by entering in 'n' number", () => {
+  expect(ceaserCipher("a")).toBe("c");
 });
